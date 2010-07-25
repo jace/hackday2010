4f052d6b325dcb1be69e51050ec1cf73892715e1
Kiran Jonnalagadda <jace@pobox.com>
2010-07-25 10:36:40 +0530

    Work-in-progress new UI

--- trafficam/__init__.py
+++ trafficam/__init__.py
@@ -41,6 +41,9 @@ def parking():
 def index():
     return render_template('index.html')
 
+@app.route('/new')
+def new():
+    return render_template('new.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
